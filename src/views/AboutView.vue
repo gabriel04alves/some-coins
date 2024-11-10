bf1877fed1859af6d044ff835a88211e5fe4ff10
Gabriel Alves <gabriel52.alves@gmail.com>
2024-11-09 22:33:59 -0300

    feat: incluindo página 'about'

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,52 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="section">
+    <div class="container" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+      <h1 class="title">Sobre o Projeto</h1>
+      <div class="content">
+        <p>
+          O <strong>Some Coins</strong> foi desenvolvido para fins de estudo. O site fornece um app de conversão
+          de moedas de forma simples e rápida consumindo a <a
+            href="https://docs.awesomeapi.com.br/api-de-moedas">awesomeapi</a>.
+        </p>
+        <hr />
+        <h2 class="subtitle">Tecnologias Utilizadas</h2>
+        <div class="content">
+          <ul>
+            <li>
+              <span class="is-primary"><i class="fab fa-vuejs"></i> Vue.js 3</span>
+            </li>
+            <li>
+              <span class="is-info"><i class="fa-regular fa-file-code"></i> JavaScript</span>
+            </li>
+            <li>
+              <span class="is-link"><i class="fab fa-css3-alt"></i> Bulma CSS</span>
+            </li>
+          </ul>
+        </div>
+        <hr />
+        <h2 class="subtitle">Contatos</h2>
+        <div class="buttons">
+          <a href="https://github.com/gabriel04alves/some-coins" target="_blank" rel="noopener noreferrer"
+            class="button is-dark">
+            <span class="icon">
+              <i class="fab fa-github"></i>
+            </span>
+            <span>GitHub</span>
+          </a>
+          <a href="https://www.linkedin.com/in/gabriel04alves" target="_blank" rel="noopener noreferrer"
+            class="button is-link">
+            <span class="icon">
+              <i class="fab fa-linkedin"></i>
+            </span>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script setup>
+</script>
+
+<style></style>
