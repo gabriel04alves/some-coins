6f59f2d5b07fafc3b745b56c329453249a953518
Gabriel Alves <gabriel52.alves@gmail.com>
2024-11-09 22:28:12 -0300

    fix: corrigindo a responsividade

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="card is-flex is-flex-direction-column is-align-items-center p-4 "
+    <div class="card is-flex is-flex-direction-column is-align-items-center p-4 mx-4 mt-6"
         style="max-width: fit-content; justify-self: center;">
         <div class="card-header">
-            <h1 class="title">🪙 {{ result }}</h1>
+            <h1 class="title has-text-centered">🪙 {{ result }}</h1>
         </div>
         <div class="card-content is-flex is-flex-direction-column">
 
-            <div class="columns is-flex is-align-items-end">
+            <div class="columns is-flex is-align-items-end is-flex-wrap-wrap">
                 <div class="column">
                     <h2>Origem:</h2>
                     <div class="select">
                         <select v-model="fromCurrency">
+                            <option value="" disabled selected>Selecione a moeda de origem</option>
                             <option v-for="(currency, code) in currencies" :key="code" :value="code">{{
                                 currency.name.split('/')[0].trim() }}
                             </option>
@@ -19,13 +20,15 @@
                 </div>
 
                 <div class="column">
-                    <input class="input" style="max-width: 10vw;" type="number" v-model="amount" placeholder="Valor">
+                    <h2>Valor:</h2>
+                    <input class="input" type="number" min="0" v-model="amount" placeholder="Digite um valor">
                 </div>
 
                 <div class="column">
-                    <h2 class="mt-3">Destino:</h2>
+                    <h2 class="">Destino:</h2>
                     <div class="select">
                         <select class="select" v-model="toCurrency">
+                            <option value="" disabled selected>Selecione a moeda de destino</option>
                             <option v-for="(currency, code) in currencies" :key="code" :value="code">{{
                                 currency.name.split('/')[0].trim() }}</option>
                         </select>
@@ -43,6 +46,7 @@
 import { ref, onMounted } from 'vue';
 import { fetchCurrencies } from '../composables/fetchCurrencies';
 import { convertCurrency } from '../composables/currencyConverter';
+import { formatCurrency } from '@/utils/formatCurrency';
 
 const fromCurrency = ref('');
 const toCurrency = ref('');
@@ -71,9 +75,9 @@ const handleConvertCurrency = async () => {
 
     try {
         let convertedValue = await convertCurrency(fromCurrency.value, toCurrency.value, amount.value);
-        result.value = `${amount.value} ${fromCurrency.value} equivalem à ${convertedValue} `;
+        result.value = `${formatCurrency(amount.value, "pt-BR", fromCurrency.value)} equivalem à ${convertedValue} `;
     } catch (error) {
-        result.value = "Erro ao converter, tente novamente...";
+        result.value = "Erro ao converter, tente outra conversão...";
         console.error("Erro ao converter a moeda:", error);
     }
 }
